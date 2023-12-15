045a62c55bcf5edfa2046b1b453784f962ac4e49
Akshay Mourya <akshay786lud@gmail.com>
2023-12-15 22:59:45 +0530

    hi

---
+++ stack.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include <stack>
+using namespace std;
+int main() {
+	stack<int> stack;
+	stack.push(21);// The values pushed in the stack should be of the same data which is written during declaration of stack
+	stack.push(22);
+	stack.push(24);
+	stack.push(25);
+	int num=0;
+
+	if(stack.top() == stack.empty()){
+	    cout<<"empty";
+	}
+	else if(stack.top() != stack.empty()){
+	    cout<<"not empty";
+	}
+	cout<<endl;
+	cout<<stack.size();
+		cout<<endl;
+    while(!stack.empty()){
+        cout<<" "<<stack.top();
+        stack.pop();
+    }
+	
+}
